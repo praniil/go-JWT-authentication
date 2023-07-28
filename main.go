f573811921d6678e2c2a4ee58553a28dfd449659
praniil <pparajuli15@gmail.com>
2023-07-28 07:52:52 +0545

    JWT authentication start

---
+++ main.go
@@ -0,0 +1,7 @@
+//JWT:= json web tokens(can be implemented in any server side programming language (backend))
+//we can use golang-jwt package that provides functionality for generating and validating JWTs
+package main
+
+import(
+	"github.com/golang-jwt/jwt"
+)
