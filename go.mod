bc024f73d368d6c634aa149f17c000dac62887bc
praniil <pparajuli15@gmail.com>
2023-07-29 11:21:07 +0545

    updated main .go

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-jwt-authentication
 
 go 1.19
 
-require github.com/golang-jwt/jwt v3.2.2+incompatible
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/golang-jwt/jwt/v5 v5.0.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
-//JWT:= json web tokens(can be implemented in any server side programming language (backend))
-//we can use golang-jwt package that provides functionality for generating and validating JWTs
+// JWT:= json web tokens(can be implemented in any server side programming language (backend))
+// we can use golang-jwt package that provides functionality for generating and validating JWTs
+// jwt made of 3 parts seperated by "."
+// (information like algorithm used to generate the signature).(application specific information like username).(signature)		//JWT
 package main
 
-import(
-	"github.com/golang-jwt/jwt"
+import (
+	"log"
+	"net/http"
+	"handlers/handlers.go"
 )
+
+func main(){
+	//handlers needed
+	http.HandleFunc("/signin", Signin)
+	// http.HandleFunc("/welcome", Welcome)
+	// http.HandleFunc("/refresh", Refresh)
+	// http.HandleFunc("/logout", Logout)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
